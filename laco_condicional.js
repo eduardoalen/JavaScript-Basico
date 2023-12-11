0d2a6ef13e136aecbe7bf7977a4ddd6adc467d8d
Eduardo Alencar <alencareduardo17@gmail.com>
2023-12-11 14:29:18 -0300

    Adicionando o while

--- laco_condicional.js
+++ laco_condicional.js
@@ -1,1 +1,14 @@
+const input = require("readline-sync");
+
 const numero_sorteado = 5;
+
+let numero = input.question("Qual numero voce escolhe? ");
+
+console.log(numero, typeof numero);
+
+while (numero != numero_sorteado) {
+  console.log('Voce errou o numero, tente novamente...');
+  numero = input.question("Qual numero voce escolhe? ");
+}
+
+console.log('Voce acertouuu')
